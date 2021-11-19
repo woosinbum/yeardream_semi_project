845aa0d88a1d321abef803345fd438875141c4c9
Ch Joa <chipmunkskida@gmail.com>
2021-11-19 05:49:13 +0000

    Firefox driver

--- python/real-estate-crawling.py
+++ python/real-estate-crawling.py
@@ -1,14 +1,14 @@
+from pyvirtualdisplay import Display
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException, \
+                                        StaleElementReferenceException
 import pymongo
 import time
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--single-process")
-s = Service('/home/ubuntu/elice-team5-projects/chromedriver')
+display = Display(visible=0, size=(800,600))
+display.start()
+
+path = '/home/ubuntu/elice-team5-projects/geckodriver'
 
 # mongodb에서 상권 좌표 가져오기
 atlas_url = "mongodb+srv://user1:uZGuuMyRngM3izgG@cluster0.cu0c3.mongodb.net"
@@ -20,75 +20,102 @@ collection = db.get_collection("area_info")
 collection2 = db.get_collection("real_estate")
 collection2.delete_many({})
 
-naver_land_uri = "https://new.land.naver.com/offices?"
+naver_land = "https://new.land.naver.com/offices?"
 
 # 상권 좌표로 검색한 페이지에서 크롤링
-with webdriver.Chrome(service=s, options=chrome_options) as driver:
+with webdriver.Firefox(executable_path = path) as driver:
     driver.implicitly_wait(10)
     items = collection.find()
     for item in items:
-        print("좌표:", item)
-        latitude, longitude = item['위도'], item['경도']
-        driver.get(f"{naver_land_uri}ms={latitude},{longitude}&a=SG&e=RETAIL")
+        try:
+            print("***************************************************************")
+            print(item['상권_코드'], item['상권_코드_명'], item['위도'], item['경도'])
+            latitude, longitude = item['위도'], item['경도']
+            driver.get(f"{naver_land}ms={latitude},{longitude}&a=SG&e=RETAIL")
 
-        # 상가 매물 리스트 스크롤 끝까지 내리기
-        SCROLL_PAUSE_SEC = 1
+            # # 상가 매물 리스트 스크롤 끝까지 내리기
+            SCROLL_PAUSE_SEC = 2
 
-        building_list = driver.find_element_by_xpath(
-            '//*[@id="listContents1"]/div'
-            )
-
-        last_height = driver.execute_script(
-            'return arguments[0].scrollHeight',
-            building_list
-            )
-        while True:
-            driver.execute_script(
-                'arguments[0].scrollTop = arguments[0].scrollHeight',
-                building_list
+            building_list = driver.find_element_by_xpath(
+                '//*[@id="listContents1"]/div'
                 )
 
-            time.sleep(SCROLL_PAUSE_SEC)
-            new_height = driver.execute_script(
+            last_height = driver.execute_script(
                 'return arguments[0].scrollHeight',
                 building_list
                 )
+            for j in range(10):
+                driver.execute_script(
+                    'arguments[0].scrollTop = arguments[0].scrollHeight',
+                    building_list
+                    )
 
-            if new_height == last_height:
                 time.sleep(SCROLL_PAUSE_SEC)
+                new_height = driver.execute_script(
+                    'return arguments[0].scrollHeight',
+                    building_list
+                    )
+
                 if new_height == last_height:
-                    break
-            last_height = new_height
-        # 다 내린 후에는 하나씩 클릭해서 정보 읽어오기
-        link_list = driver.find_elements_by_xpath(
-            '//*[@id="listContents1"]//a[@class="item_link"]'
-            )
-        data = []
-        for i in range(len(link_list)):
-            title = link_list[i].find_element_by_xpath('div[1]/span')
-            bill_type_list = link_list[i].find_element_by_xpath(
-                'div[2]/span[1]'
+                    time.sleep(SCROLL_PAUSE_SEC)
+                    if new_height == last_height:
+                        break
+                last_height = new_height
+            print("화면 높이 :",last_height)
+            # 다 내린 후에는 하나씩 클릭해서 정보 읽어오기
+            link_list = driver.find_elements_by_xpath(
+                '//*[@id="listContents1"]//a[@class="item_link"]'
                 )
-            bill_amount_list = link_list[i].find_element_by_xpath(
-                'div[2]/span[2]'
-                )
-            info = {}
-            info['분류'] = title.text
-            info['거래방식'] = bill_type_list.text
-            info['가격'] = bill_amount_list.text
-            link_list[i].click()
-            table = driver.find_element_by_xpath(
-                '//*[@id="detailContents1"]/div[1]/table/tbody'
-                )
-            keys = table.find_elements_by_tag_name('th')
-            values = table.find_elements_by_tag_name('td')
-
-            for key, value in zip(keys, values):
-                if key.text not in ['매물설명']:
-                    info[key.text] = value.text
-            data.append(info)
-        print("링크 수:", len(link_list))
-        print("생성된 데이터:", len(data))
-
+            data = []
+            for i in range(len(link_list)):
+                title = link_list[i].find_element_by_xpath('div[1]/span')
+                bill_type_list = link_list[i].find_element_by_xpath(
+                    'div[2]/span[1]'
+                    )
+                bill_amount_list = link_list[i].find_element_by_xpath(
+                    'div[2]/span[2]'
+                    )
+                info = {}
+                info['분류'] = title.text
+                info['거래방식'] = bill_type_list.text
+                info['가격'] = bill_amount_list.text
+                link_list[i].click()
+                try:
+                    table = driver.find_element_by_xpath(
+                        '//*[@id="detailContents1"]/div[1]/table/tbody'
+                        )
+                    keys = table.find_elements_by_tag_name('th')
+                    values = table.find_elements_by_tag_name('td')
+                    for key, value in zip(keys, values):
+                        if key.text not in ['매물설명']:
+                            info[key.text] = value.text
+                    if info:
+                        data.append(info)
+                except NoSuchElementException or \
+                        StaleElementReferenceException:
+                    time.sleep(3)
+                    try:
+                        table = driver.find_element_by_xpath(
+                            '//*[@id="detailContents1"]/div[1]/table/tbody'
+                            )
+                        keys = table.find_elements_by_tag_name('th')
+                        values = table.find_elements_by_tag_name('td')
+                        for key, value in zip(keys, values):
+                            if key.text not in ['매물설명']:
+                                info[key.text] = value.text
+                        if info:
+                            data.append(info)
+                    except NoSuchElementException or \
+                            StaleElementReferenceException:
+                        print('Can\'t load element')
+                print(info)
+            print("링크 수:", len(link_list))
+            print("생성된 데이터:", len(data))
+        except NoSuchElementException or \
+                StaleElementReferenceException:
+            print("실패")
         # mongodb에 적재
-        collection2.insert_many(data)
+        if data:
+            collection2.insert_many(data)
+
+display.stop()
