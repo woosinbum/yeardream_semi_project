6b3db1fec30080cdafb9f062aaf75b74c4c036e4
woosinbeom <tlsqja0127@naver.com>
2021-11-22 23:04:49 +0900

    차트구현

--- flask/app/static/js/main.js
+++ flask/app/static/js/main.js
@@ -2,69 +2,184 @@
 function modal(x) {
     document.getElementById("modal_on").style.display=x;
 }
+// 모든업종 연도별 평균매출
+let sales = document.getElementById('sales_bar').getContext('2d');
 
-Highcharts.chart('chart', {
-    chart: {
-        type: 'column'
+let barChart = new Chart(sales, {
+    type: 'bar',
+    data: {
+        labels: ['2017','2018','2019','2020','2021'], 
+        datasets:[{
+            data: [
+                700,
+                600,
+                600,
+                500,
+                300
+            ],
+            backgroundColor:[
+                '#0367A6',
+                '#BDDEF2',
+                '#F2B705',
+                '#F28705',
+                '#F25C05'
+            ],
+            borderWidth:1,
+            borderColor : '#f0f0f0',
+            hoverBorderWidth:4,    
+        }]
+
     },
-    title: {
-        text: 'Monthly Average Rainfall'
-    },
-    subtitle: {
-        text: 'Source: WorldClimate.com'
-    },
-    xAxis: {
-        categories: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-            'Oct',
-            'Nov',
-            'Dec'
-        ],
-        crosshair: true
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Rainfall (mm)'
+    options: {
+        responsive: false,
+        scales: {
+            x: {
+                grid: {
+                    display: false,
+                }
+            },
+            y: {
+                grid: {
+                    display:false
+                }
+            }
+        },
+        plugins: {
+            title: {
+                display:false,
+            },
+            legend :{
+                display: false,
+                position:'top',
+             },
+            tooltips: {
+                enabled: true,
+            },
+            layout: {
+                top:10,
+                bottom: 10
+            },
         }
+    }
+
+
+})
+// 폐업률 데이터 망한가게/전체가게수
+let closed = document.getElementById('closed_bar').getContext('2d');
+
+let closedBarChart = new Chart(closed, {
+    type: 'bar',
+    data: {
+        labels: ['2017','2018','2019','2020','2021'],
+        datasets:[{
+            data: [
+                60,
+                70,
+                60,
+                80,
+                90
+            ],
+            backgroundColor:[
+                '#1D435C',
+                '#11A3B3',
+                '#EFBF1F',
+                '#E2641B',
+                '#E43120'
+            ],
+            borderWidth:1,
+            borderColor : '#f0f0f0',
+            hoverBorderWidth:4,    
+        }]
+
     },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
+    options: {
+        responsive: false,
+        scales: {
+            x: {
+                grid: {
+                    display: false,
+                }
+            },
+            y: {
+                grid: {
+                    display:false
+                }
+            }
+        },
+        plugins: {
+
+            title: {
+                display:false,
+            },
+            legend :{
+                display: false,
+                position:'top',
+             },
+            tooltips: {
+                enabled: true,
+            },
+            layout: {
+                top:10,
+                bottom: 10
+            },
         }
+    }
+
+
+})
+
+
+
+
+// 현재 매출이 잘나오는 업종이나 상권의 트렌드 파이그래프반영 
+let trend = document.getElementById('trend_chart').getContext('2d');
+
+let pieChart = new Chart(trend, {
+    type: 'pie',
+    data: {
+        labels: ['요식업','서비스업','교육','전자제품','카페'],
+        datasets:[{
+            label: 'trend_view',
+            data: [
+                500,
+                300,
+                100,
+                300,
+                700
+            ],
+            backgroundColor:[
+                '#0367A6',
+                '#BDDEF2',
+                '#F2B705',
+                '#F28705',
+                '#F25C05'
+            ],
+            borderWidth:1,
+            borderColor : '#f0f0f0',
+            hoverBorderWidth:4,    
+        }]
+
     },
-    series: [{
-        name: 'Tokyo',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    options: {
+        responsive: false,
+        plugins: {
+            title: {
+                display: true,
+                text: 'trend',
+                fontsize: 10,
+            },
+            legend :{
+                display: true,
+                position:'top',
+             },
+            tooltips: {
+                enabled: true,
+            },
+            layout: {
+                top:10
+            },
+        }
+    }
 
-    }, {
-        name: 'New York',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
 
-    }, {
-        name: 'London',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-    }, {
-        name: 'Berlin',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
-    }]
-});
+})
